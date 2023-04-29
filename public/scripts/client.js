b173d384cc568b3f66495bb11d2cb3211fd4b5f2
porchsal <porchsal@yahoo.com>
2023-04-29 15:03:46 -0400

    client.js form Validation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,8 +7,9 @@
 // Fake data taken from initial-tweets.json
 const data = [];
 $( document ).ready(function() {
-    const createTweetElement = function(tweetData) {
-  
+
+//  creates tweet element
+  const createTweetElement = function(tweetData) {
     const $tweet = `
         <article class="tweet">
             <header class="tweet-header">
@@ -19,7 +20,7 @@ $( document ).ready(function() {
               <p>${tweetData.content.text}</p>
             </section>
             <footer class="tweet-footer-container">
-              <span class="time-created">${timeago.format(tweetData.created_at)}</span>
+              <p class="time-created">${timeago.format(tweetData.created_at)}</p>
                 <div class="icons">
                   <i class="fas fa-flag"></i>
                   <i class="fas fa-retweet"></i>
@@ -29,38 +30,55 @@ $( document ).ready(function() {
           </article>
         `
      return $tweet;
-    };
-      
-    const renderTweets = function(tweets) {
-      $('#tweets-container').empty();
-      for (let tweet of tweets){
-        const $tweet = createTweetElement(tweet);
-        $('#tweets-container').append($tweet)
-      }
-    };
-    
-    //renderTweets(data);
-    
-    $(".tweet-target").submit(function (event) {
-        event.preventDefault();
-        const formTweet = $(this).serialize;
-        $.post("/tweets/", formTweet, () => {
-             $(this).find("#tweet-text").val("");
-             $(this).find(".counter").val("");
-           //renderTweets(formTweet);
-            
-        }
+  };
 
-        )
+//renderTweets(data);  
+  const renderTweets = function(tweets) {
+    $('#tweets-container').empty();
+    for (let tweet of tweets){
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet)
+    }
+  };
 
-    })
-
-
+// request to load tweets to #tweets-container 
   const loadTweets = function(){
     $.get("/tweets/", function(newTweet) {
       renderTweets(newTweet.reverse());
     })
-  }
+  } 
+//  add new tweet when submit    
+    $("#tweet-target").on("submit", function (event) {
+      event.preventDefault();
+      const maxChar = 140;
+      const inputCounter = $(this).find(".counter").val(); 
+      const inputText =  $(this).find("#tweet-text").val();
+      
+      if( !inputText ){
+        alert("Tweet empty");
+      } else if (inputCounter < 0) {
+        alert("Tweet too long, 140 characters max");
+      } else {
+        const formTweet = $(this).serialize();
+        $.ajax({
+          url: "/tweets/",
+          method: 'POST',
+          data: formTweet,
+          success: function(){
+            // $(this).find("#tweet-text").val();
+            // $(this).find(".counter").val();
+            inputText;
+            inputCounter;
+            loadTweets();
+          }
+
+        })
+           
+      }
+      
+    
+    });
+
 
   loadTweets();
 });
